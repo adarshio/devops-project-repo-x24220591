99c7bc6e68012b4d5ab6086bcbf13d78fe582970
EC2 Default User <ec2-user@ip-172-31-1-33.ec2.internal>
2026-03-09 12:15:06 +0000

    pylint changes

--- home/apps.py
+++ home/apps.py
@@ -5,4 +5,3 @@ class HomeConfig(AppConfig):
     """Create app here"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
-
--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class BasicTests(TestCase):
+
+    def test_admin_login_page(self):
+        response = self.client.get("/admin/login/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_redirect(self):
+        response = self.client.get("/admin/")
+        self.assertEqual(response.status_code, 302)
+
+    def test_invalid_page(self):
+        response = self.client.get("/invalid-url/")
+        self.assertEqual(response.status_code, 404)
+
+    def test_admin_logout_redirect(self):
+        response = self.client.get("/admin/logout/")
+        self.assertIn(response.status_code, [200, 302])
+
+    def test_admin_password_reset(self):
+        response = self.client.get("/admin/password_reset/")
+        self.assertIn(response.status_code, [200, 302])
+        
